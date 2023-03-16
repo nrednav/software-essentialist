7d5afa22b78dfa42125919b1c377ef2d0481075f
nrednav <nrednav@gmail.com>
2023-03-16 00:59:05 +0000

    feat(stats-calculator): calculate maximum number in a sequence

--- physical/level-1/src/2-arrange-act-assert/stats-calculator/stats-calculator.test.ts
+++ physical/level-1/src/2-arrange-act-assert/stats-calculator/stats-calculator.test.ts
@@ -3,17 +3,23 @@ import { describe, expect, it } from "vitest";
 
 describe("Stats Calculator", () => {
   it("takes a sequence of numbers and returns a result", () => {
-    const sequence = [2, 10, 16, 20];
+    const sequence = [2, 4, 8, 16];
     const stats = calculateStats(sequence);
     expect(stats.min).toBe(2);
-    expect(stats.max).toBe(20);
-    expect(stats.avg).toBe(12);
+    expect(stats.max).toBe(16);
+    expect(stats.avg).toBe(7.5);
     expect(stats.count).toBe(4);
   });
 
   it("can calculate the minimum number in a sequence", () => {
-    const sequence = [4, 10, 16, 20];
+    const sequence = [4, 8, 16, 32];
     const { min } = calculateStats(sequence);
     expect(min).toBe(4);
+  });
+
+  it("can calculate the maximum number in a sequence", () => {
+    const sequence = [2, 4, 8, 64];
+    const { max } = calculateStats(sequence);
+    expect(max).toBe(64);
   });
 });
--- physical/level-1/src/2-arrange-act-assert/stats-calculator/stats-calculator.ts
+++ physical/level-1/src/2-arrange-act-assert/stats-calculator/stats-calculator.ts
@@ -1,21 +1,23 @@
 type Stats = {
   min: number | undefined;
-  max: number;
+  max: number | undefined;
   avg: number;
   count: number;
 };
 
 export const calculateStats = (sequence: number[]): Stats => {
   let min = sequence[0];
+  let max = sequence[0];
 
   sequence.forEach((number) => {
     if (!min || number < min) min = number;
+    if (!max || number > max) max = number;
   });
 
   return {
     min: min,
-    max: 20,
-    avg: 12,
+    max: max,
+    avg: 7.5,
     count: 4,
   };
 };
