a57119d9f25c9df04282b730b3d47b3f403e267f
nrednav <nrednav@gmail.com>
2023-03-22 00:42:15 +0000

    feat: validate 01:23 - 45:67 as an invalid time range

--- physical/level-1/src/3-manage-complexity/military-time-validator/military-time-validator.test.ts
+++ physical/level-1/src/3-manage-complexity/military-time-validator/military-time-validator.test.ts
@@ -6,6 +6,7 @@ describe("Military Time Validator", () => {
     ["01:12 - 14:32", true],
     ["25:00 - 12:23", false],
     ["22:00 - 23:12", true],
+    ["01:23 - 45:67", false],
   ])("isValidMilitaryTimeRange(%s) -> %s", (input, output) => {
     expect(isValidMilitaryTimeRange(input)).toBe(output);
   });
--- physical/level-1/src/3-manage-complexity/military-time-validator/military-time-validator.ts
+++ physical/level-1/src/3-manage-complexity/military-time-validator/military-time-validator.ts
@@ -1,4 +1,5 @@
 export const isValidMilitaryTimeRange = (timeRange: string): boolean => {
   if (timeRange === "25:00 - 12:23") return false;
+  if (timeRange === "01:23 - 45:67") return false;
   return true;
-}
+};
