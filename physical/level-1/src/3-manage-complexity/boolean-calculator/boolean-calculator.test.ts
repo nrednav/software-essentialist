af368550c36b8f47bd05c760897a50773e630687
nrednav <nrednav@gmail.com>
2023-03-23 02:18:13 +0000

    refactor: dedup one aspect of the tests

--- physical/level-1/src/3-manage-complexity/boolean-calculator/boolean-calculator.test.ts
+++ physical/level-1/src/3-manage-complexity/boolean-calculator/boolean-calculator.test.ts
@@ -1,69 +1,57 @@
 import { BooleanCalculator } from "./boolean-calculator";
-import { describe, expect, it } from "vitest";
+import { describe, expect, test } from "vitest";
+
+const evaluateTestCase = (input: string, expected: boolean) => {
+  expect(BooleanCalculator.evaluate(input)).toBe(expected);
+};
 
 describe("Boolean Calculator", () => {
-  it("can evaluate single value expressions", () => {
-    const inputs = ["TRUE", "FALSE"];
-    const outputs = inputs.map(BooleanCalculator.evaluate);
-    expect(outputs[0]).toBe(true);
-    expect(outputs[1]).toBe(false);
+  describe("Evaluation of single value expressions", () => {
+    test.each([
+      ["TRUE", true],
+      ["FALSE", false],
+    ])("evaluate(%s) -> %s", evaluateTestCase);
   });
 
-  it("can evaluate expressions with the NOT operator", () => {
-    const inputs = ["NOT TRUE", "NOT FALSE"];
-    const outputs = inputs.map(BooleanCalculator.evaluate);
-    expect(outputs[0]).toBe(false);
-    expect(outputs[1]).toBe(true);
+  describe("Evaluation of expressions with the NOT operator", () => {
+    test.each([
+      ["NOT TRUE", false],
+      ["NOT FALSE", true],
+    ])("evaluate(%s) -> %s", evaluateTestCase);
   });
 
-  it("can evaluate expressions with the AND operator", () => {
-    const testCases = [
+  describe("Evaluation of expressions with the AND operator", () => {
+    test.each([
       ["FALSE AND FALSE", false],
       ["FALSE AND TRUE", false],
       ["TRUE AND FALSE", false],
       ["TRUE AND TRUE", true],
-    ] as const;
-
-    testCases.forEach(([input, expected]) =>
-      expect(BooleanCalculator.evaluate(input)).toBe(expected)
-    );
+    ])("evaluate(%s) -> %s", evaluateTestCase);
   });
 
-  it("can evaluate expressions with the OR operator", () => {
-    const testCases = [
+  describe("Evaluation of expressions with the OR operator", () => {
+    test.each([
       ["FALSE OR FALSE", false],
       ["FALSE OR TRUE", true],
       ["TRUE OR FALSE", true],
       ["TRUE OR TRUE", true],
-    ] as const;
-
-    testCases.forEach(([input, expected]) =>
-      expect(BooleanCalculator.evaluate(input)).toBe(expected)
-    );
+    ])("evaluate(%s) -> %s", evaluateTestCase);
   });
 
-  it("can evaluate expressions with multiple operators combined", () => {
-    const testCases = [
+  describe("Evaluation of expressions with multiple operators combined", () => {
+    test.each([
       ["TRUE OR TRUE OR TRUE AND FALSE", true],
       ["TRUE OR FALSE AND NOT FALSE", true],
-    ] as const;
-
-    testCases.forEach(([input, expected]) =>
-      expect(BooleanCalculator.evaluate(input)).toBe(expected)
-    );
+    ])("evaluate(%s) -> %s", evaluateTestCase);
   });
 
-  it("can evaluate expressions with parentheses", () => {
-    const testCases = [
+  describe("Evaluation of expressions with parentheses", () => {
+    test.each([
       ["(TRUE OR TRUE OR TRUE) AND FALSE", false],
       ["NOT (TRUE AND TRUE)", false],
       ["(TRUE AND FALSE) OR (TRUE AND TRUE)", true],
       ["(TRUE AND FALSE OR TRUE) OR (TRUE AND TRUE AND FALSE)", true],
       ["(FALSE OR FALSE) AND (TRUE OR FALSE)", false],
-    ] as const;
-
-    testCases.forEach(([input, expected]) =>
-      expect(BooleanCalculator.evaluate(input)).toBe(expected)
-    );
+    ])("evaluate(%s) -> %s", evaluateTestCase);
   });
 });
